95ede8f0241ff7dd160ad7c4efcb3394e1be2920
Aiden <fsd76005588@gmail.com>
2021-02-26 08:40:43 +0900

    edit payservice

--- src/services/PayService.js
+++ src/services/PayService.js
@@ -1,73 +1,73 @@
-const ElasticsearchService = require('../interfaces/ElasticsearchService')
+const ElasticsearchService = require('../interfaces/ElasticsearchService');
 
 class UserService extends ElasticsearchService {
-  static index = ['local-pay-1']
+  static index = ['local-pay-1'];
 
   /**
    * [constructor description]
    * @return {[type]} options.router     [description]
    */
   constructor({ router }) {
-    super()
+    super();
     // Controller, Service 동시에
-    router.get('/api/pay', (req, res, next) => this.searchPay(req, res).catch(next))
+    router.get('/api/pay', (req, res, next) =>
+      this.searchPay(req, res).catch(next)
+    );
 
-    this.router = router
+    this.router = router;
   }
 
   async searchPay(req, res) {
-    console.log('GET axios22222')
+    console.log('GET axios22222');
 
-    const { size, region, category } = req.query
+    const { size, region, category,from } = req.query;
     // /api/pay?size=20&region=가평군&....
-    
+
     // console.log("req.querybnaskldnaksndlks",req)
     // region = rows.
     const elasticQuery = {
-        bool:{
-            must:[
-            {"match" : {"시군명" : region}},
-            {"match" : {"업종명(종목명)": category}}
-            ]
-          }
-    //  select * from 'index table' where 시군명="" and 업종명(종목명)= "일반휴게음식-일반한식"
-    }
+      bool: {
+        must: [
+          { match: { 시군명: region } },
+          { match: { '업종명(종목명)': category } },
+        ],
+      },
+      //  select * from 'index table' where 시군명="" and 업종명(종목명)= "일반휴게음식-일반한식"
+    };
     // const {
     //     body:{hits:rows},
     // }=await this.elastic.search({
     //     index: UserService.index,
-        
+
     // })
     const {
       body: {
         hits: { hits: rows },
-        aggregations
+        aggregations,
       },
     } = await this.elastic.search({
       index: UserService.index,
-    //   region :region ,
-    //   category:category,
-    
-      size: size ? size : 20, // default : 10
+
+      from : from ? from : 0, //쪽수
+      size: size ? size : 20, // default : 10 // 한번에 나타나는 데이터
       body: {
         query: elasticQuery,
         aggs: {
-            '시군명 요약': {
-              terms: {
-                field: '시군명',
-              },
+          '시군명 요약': {
+            terms: {
+              field: '시군명',
             },
-            '업종별 요약': {
-                terms: {
-                  field: '업종명(종목명)',
-                },
-              },
           },
-    
+          '업종별 요약': {
+            terms: {
+              field: '업종명(종목명)',
+            },
+          },
+        },
       },
-    })
+    });
 
-    console.log("rows222222", aggregations)
+    console.log('rows222222', aggregations);
 
     const {
       body: { count: total },
@@ -76,11 +76,10 @@ class UserService extends ElasticsearchService {
       body: {
         query: elasticQuery,
       },
-    })
+    });
 
-    res.send({ rows, total, aggregations})
+    res.send({ rows, total, aggregations });
   }
-
 }
 
-module.exports = UserService
+module.exports = UserService;
